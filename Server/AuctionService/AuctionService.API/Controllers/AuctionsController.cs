338b9aaae16794a2fd21f51a1b10ee67ab180dcf
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-03-08 01:17:21 +0700

    Initial commit!

--- Server/AuctionService/AuctionService.API/Controllers/AuctionsController.cs
+++ Server/AuctionService/AuctionService.API/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using AuctionService.API.DTOs;
 using AuctionService.API.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,11 +22,16 @@ namespace AuctionService.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllAuctions()
+        public async Task<IActionResult> GetAllAuctions(string date)
         {
-            var auctions = await _context.Auctions.Include(x => x.Item).OrderBy(x => x.Item.Make).ToListAsync();
+            var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-            return Ok(_mapper.Map<List<AuctionDto>>(auctions));
+            if (!string.IsNullOrEmpty(date))
+            {
+                query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+            }
+
+            return Ok(await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync());
         }
 
         [HttpGet("{id}")]
---
+++ Server/SearchService/SearchService.API/Program.cs
@@ -0,0 +1,40 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.API.Data;
+using SearchService.API.Models;
+using SearchService.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
